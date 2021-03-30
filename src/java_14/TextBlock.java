02a9ee25f84083f417e74c7e5593f24bdeefb5e9
mjedli <jedli.mejdi.recup@gmail.com>
2021-03-30 17:49:59 +0200

    null pointer execption

--- src/java_14/TextBlock.java
+++ src/java_14/TextBlock.java
@@ -42,6 +42,20 @@ public class TextBlock {
                 """;
 		
 		System.out.println(text); // test test 2 test 3
+		
+		String text2 = """
+                test \s
+                test 2 \s
+                test 3 \s
+                """;
+		
+		System.out.println(text2); // test test 2 test 3
+		
+		/*
+			test  
+test 2  
+test 3 
+		 */
 
 	}
 
