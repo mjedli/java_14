6e20202e19d67661e2650062002ae2d16bdc95c9
mjedli <jedli.mejdi.recup@gmail.com>
2021-03-30 18:54:41 +0200

    null pointer execption

--- src/java_14/NullPointerExceptions.java
+++ src/java_14/NullPointerExceptions.java
@@ -19,16 +19,14 @@ public class NullPointerExceptions {
 		
 		//JEP 358 : Helpful NullPointerExceptions
 		
-		StringBuilder sb = new StringBuilder
+		StringBuilder sb = new StringBuilder();
 		sb.append("test").read();
 		
 		/*
-		 * 	Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
-			Syntax error, insert "( )" to complete ClassInstanceCreationExpression
-			Syntax error, insert ";" to complete BlockStatements
+			Exception in thread "main" java.lang.Error: Unresolved compilation problem: 
 			The method read() is undefined for the type StringBuilder
 
-			at java_14/java_14.All.main(All.java:19)
+			at java_14.NullPointerExceptions.main(NullPointerExceptions.java:23)
 		 */
 		
 
