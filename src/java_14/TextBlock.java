c81876da90f027f228586a1d3fbb5bde7d426160
mjedli <jedli.mejdi.recup@gmail.com>
2021-03-30 17:41:19 +0200

    null pointer execption

---
+++ src/java_14/TextBlock.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package java_14;
+
+/**
+ * @author mjedli
+ *
+ */
+public class TextBlock {
+
+	/**
+	 * @param args
+	 * 
+	 * Text Blocks (Second Preview)
+	 * 
+	 */
+	public static void main(String[] args) {
+	
+		String html = """
+	              <html>
+	                  <body>
+	                      <p>Hello, world</p>
+	                  </body>
+	              </html>
+	              """;
+		
+		System.out.println(html);
+
+	}
+
+}
